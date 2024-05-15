60a1a24516a2688caadf0d16cc2a98b38ae81eb3
Tristan Goodell <tristan.goodell.1@us.af.mil>
2024-05-15 16:07:53 -0400

    Using Histoire now. Primevue button documentation started

---
+++ storybook-eval/stories/Button.story.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { reactive, ref } from 'vue'
+import Button from 'primevue/button';
+
+const state = reactive({
+  style: null,
+  disabled: false,
+  content: 'Hello world',
+})
+
+const message = ref('Meow!')
+</script>
+
+<template>
+  <Story>
+    <Variant>
+      <Button 
+        :style="state.style" 
+        :label="state.content" 
+        :disabled="state.disabled" 
+      />
+
+      <template #controls>
+        Style: <input type="text" v-model="state.style" />
+        Content: <input type="text" v-model="state.content" />
+        Disabled: <input type="checkbox" v-model="state.disabled" />
+      </template>
+    </Variant>
+  </Story>
+</template>
