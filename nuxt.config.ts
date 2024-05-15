6933e3ea63f0c57aa261ae87f8e87638869488d1
Tristan Goodell <tristan.goodell.1@us.af.mil>
2024-05-15 15:09:31 -0400

    Added Storyboard. Current version is broken, so rolled back to 7.0.2. Still not working yet.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/storybook"],
+  storybook: {
+    url: 'http://localhost:6006',
+    port: 6006,
+  },
 })
