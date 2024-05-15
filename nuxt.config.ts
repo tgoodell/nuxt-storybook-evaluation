acd5c53d5949539a22edd7a8105980c60275b189
Tristan Goodell <tristan.goodell.1@us.af.mil>
2024-05-15 15:44:54 -0400

    Remove Storybook

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/storybook"],
-  storybook: {
-    url: 'http://localhost:6006',
-    port: 6006,
-  },
 })
